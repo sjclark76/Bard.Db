429c000e6365f012b95740f5cf865166bc81b991
Stuart Clark <sjclark76@gmail.com>
2020-08-20 11:12:49 +1200

    make internal fields public

--- Bard.Db/BardDbBase.cs
+++ Bard.Db/BardDbBase.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Docker.DotNet;
 using Docker.DotNet.Models;
@@ -9,15 +10,12 @@ using Newtonsoft.Json;
 namespace Bard.Db
 {
     /// <summary>
-    /// BardDb Db class to provide base functionality (Internal Use)
+    ///     BardDb Db class to provide base functionality (Internal Use)
     /// </summary>
     public abstract class BardDbBase
     {
-        private protected readonly string DatabaseName;
-        private protected readonly string PortNumber;
-        private protected readonly string ImageName;
-        private protected readonly string TagName;
         private protected readonly DockerClient DockerClient;
+
         private string _dbContainerId = null!;
 
         internal BardDbBase(string databaseName, string portNumber, string imageName, string tagName)
@@ -29,31 +27,57 @@ namespace Bard.Db
             DockerClient = new DockerClientConfiguration().CreateClient();
         }
 
+        
         /// <summary>
-        /// Start the database
-        /// <remarks>Will download the image and create the container if it does not exist already.</remarks>
+        /// The database name
+        /// </summary>
+        public string DatabaseName { get; }
+        
+        /// <summary>
+        /// The external Port Number for the database
+        /// </summary>
+        public string PortNumber { get; }
+        
+        /// <summary>
+        /// The docker Image Name used for the database
+        /// </summary>
+        public string ImageName { get; }
+        
+        /// <summary>
+        /// The Image Tag used for the database
+        /// </summary>
+        public string TagName { get; }
+
+        /// <summary>
+        ///     Is the host platform Windows or Linux.
+        /// </summary>
+        public bool IsWindowsHost => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        /// <summary>
+        ///     Start the database
+        ///     <remarks>Will download the image and create the container if it does not exist already.</remarks>
         /// </summary>
         /// <returns>The IP address of the host machine.</returns>
         public string StartDatabase()
         {
             var result = AsyncHelper.RunSync(StartDatabaseAsync);
-        
+
             return result;
         }
-        
+
         /// <summary>
-        /// Stops the database
-        /// <remarks>Will not remove any docker containers or images</remarks>
+        ///     Stops the database
+        ///     <remarks>Will not remove any docker containers or images</remarks>
         /// </summary>
         /// <returns>True if successful</returns>
         public bool StopDatabase()
         {
             return AsyncHelper.RunSync(StopDatabaseAsync);
         }
-        
+
         /// <summary>
-        /// Stops the database Async
-        /// <remarks>Will not remove any docker containers or images</remarks>
+        ///     Stops the database Async
+        ///     <remarks>Will not remove any docker containers or images</remarks>
         /// </summary>
         /// <returns>True if successful</returns>
         public Task<bool> StopDatabaseAsync()
@@ -62,10 +86,10 @@ namespace Bard.Db
 
             return DockerClient.Containers.StopContainerAsync(_dbContainerId, new ContainerStopParameters());
         }
-        
+
         /// <summary>
-        /// Start the database Async
-        /// <remarks>Will download the image and create the container if it does not exist already.</remarks>
+        ///     Start the database Async
+        ///     <remarks>Will download the image and create the container if it does not exist already.</remarks>
         /// </summary>
         /// <returns>The IP address of the host machine.</returns>
         public async Task<string> StartDatabaseAsync()
@@ -95,17 +119,14 @@ namespace Bard.Db
                         FromImage = ImageName,
                         Tag = TagName
                     },
-                    null, new Progress<JSONMessage>(message =>
-                    {
-                        Console.WriteLine(message.Status);
-                    }));
+                    null, new Progress<JSONMessage>(message => { Console.WriteLine(message.Status); }));
             }
         }
-        
+
         private protected async Task<ContainerListResponse?> RetrieveContainer()
         {
             var fullImage = $"{ImageName}:{TagName}";
-            
+
             var containers = await DockerClient.Containers.ListContainersAsync(new ContainersListParameters
             {
                 All = true,
@@ -116,12 +137,9 @@ namespace Bard.Db
             });
 
             var matchingContainer = containers.FirstOrDefault(response => response.Image != fullImage);
-            
-            if (matchingContainer != null)
-            {
-                ThrowDuplicateContainerException(fullImage, matchingContainer);
-            }
-            
+
+            if (matchingContainer != null) ThrowDuplicateContainerException(fullImage, matchingContainer);
+
             var testDb = containers.SingleOrDefault(response => response.Image == fullImage);
 
             return testDb;
@@ -130,15 +148,15 @@ namespace Bard.Db
         private void ThrowDuplicateContainerException(string fullImage, ContainerListResponse matchingContainers)
         {
             throw new BardException($"Unable to start Container with name: {DatabaseName} for image {fullImage}" +
-                                    $"because a container already exists with that name for a different image." +
+                                    "because a container already exists with that name for a different image." +
                                     $"{Environment.NewLine}{Environment.NewLine}" +
-                                    $"Either rename the database for this image or delete the old container." +
+                                    "Either rename the database for this image or delete the old container." +
                                     $"{Environment.NewLine}{Environment.NewLine}" +
-                                    $"Run this command from the command line to view the offending container" +
+                                    "Run this command from the command line to view the offending container" +
                                     $"{Environment.NewLine}{Environment.NewLine}" +
                                     $"docker ps -a -f name={DatabaseName}" +
                                     $"{Environment.NewLine}{Environment.NewLine}" +
-                                    $"And this command to remove it." +
+                                    "And this command to remove it." +
                                     $"{Environment.NewLine}{Environment.NewLine}" +
                                     $"docker rm {matchingContainers.ID}" +
                                     $"{Environment.NewLine}"
@@ -152,7 +170,7 @@ namespace Bard.Db
             await DockerClient.Containers.StartContainerAsync(dbContainerId, new ContainerStartParameters());
 
             var container = await DockerClient.Containers.InspectContainerAsync(dbContainerId);
-            
+
             while (container.State.Health.Status == "starting")
             {
                 Console.WriteLine($"Container Status {container.State.Health.Status}");
@@ -164,7 +182,7 @@ namespace Bard.Db
 
                 container = await DockerClient.Containers.InspectContainerAsync(dbContainerId);
             }
-            
+
             Console.WriteLine($"Container Started {container.State.Health.Status}");
             Console.WriteLine($"Container Started {JsonConvert.SerializeObject(container, Formatting.Indented)}");
 
